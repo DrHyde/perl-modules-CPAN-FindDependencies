f7d31d6bfcbc4f0a293d0553debb66af01f0e38b
David Cantrell <david@cantrell.org.uk>
2009-04-15 12:17:23 +0100

    Fix failing scripts test, by removing 'use CPAN::Config'
    wasn't being used anyway, and might not exist

--- lib/CPAN/FindDependencies.pm
+++ lib/CPAN/FindDependencies.pm
@@ -18,7 +18,7 @@ require Exporter;
 @ISA = qw(Exporter);
 @EXPORT_OK = qw(finddeps);
 
-$VERSION = '2.2';
+$VERSION = '2.21';
 
 use constant DEFAULT02PACKAGES => 'http://www.cpan.org/modules/02packages.details.txt.gz';
 use constant MAXINT => ~0;
